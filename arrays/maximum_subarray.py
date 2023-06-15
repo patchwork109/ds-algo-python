e97632c4ff9784b00537b0bc591164ff28f2bc99
Samantha Genevay <samanthagenevay@gmail.com>
2023-06-14 21:59:32 -0700

    Maximum subarray problem start

---
+++ arrays/maximum_subarray.py
@@ -0,0 +1,10 @@
+# Create a function that finds the subarraynwith the largest sum, and return its sum.
+# nums = [-2,1,-3,4,-1,2,1,-5,4] -> 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+def maximum_subarray(num_list):
+    pass
+
+maximum_subarray(nums)
