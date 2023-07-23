6042eef78f0b13e5116abc2799f200b8a84c0cb8
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-23 10:45:00 -0700

    Valid anagram problem

--- strings/valid_anagram.py
+++ strings/valid_anagram.py
@@ -1,1 +1,20 @@
 # https://leetcode.com/problems/valid-anagram/
+# Write a function where given two strings s and t, return 
+# true if t is an anagram of s, and false otherwise.
+# s = "anagram", t = "nagaram" --> true
+# s = "rat", t = "car" --> false
+
+s = "anagram"
+t = "nagaram"
+
+def valid_anagram2(s,t):
+    if len(s) != len(t):
+        return False
+    
+    return sorted(s) == sorted(t)
+
+
+print(valid_anagram2(s,t))
+    
+
+
