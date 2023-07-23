fd63d52820778a78858d705f0c63d7c49c5e750f
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-23 09:46:00 -0700

    First unique character  problem

--- strings/first_unique_char.py
+++ strings/first_unique_char.py
@@ -1,2 +1,27 @@
 # https://leetcode.com/problems/first-unique-character-in-a-string/
+# Given a string s, find the first non-repeating character in it 
+# and return its index. If it does not exist, return -1.
+# s = "leetcode" --> 0
+# s = "loveleetcode" --> 2
+# s = "aabb" --> -1
+
+s = "leetcode"
+s2 = "loveleetcode"
+s3 = "aabb"
+
+def first_unique_char(any_string):
+    char_count = {}
+
+    for char in any_string:
+        char_count[char] = char_count.get(char, 0) + 1
+
+    for index, char in enumerate(any_string):
+        if char_count[char] == 1:
+            return index
+        
+    return -1
+
+print(first_unique_char(s3))
+
+
 
