f72573f06262e1696584925e6702c6fc940d98f8
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-22 19:46:00 -0700

    First index, first occurence problem

--- strings/first_index_of_first_occurence.py
+++ strings/first_index_of_first_occurence.py
@@ -1,1 +1,18 @@
 # https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+# Given two strings needle and haystack, return the index of the first occurrence of 
+# needle in haystack, or -1 if needle is not part of haystack.
+# haystack = "sadbutsad", needle = "sad" --> 0 (the first occurrence is at index 0)
+# haystack = "leetcode", needle = "leeto" --> -1 (leeto is not part of leetcode)
+
+haystack = "hellosadbutsad"
+needle = "sad"
+
+def index_of_first_occurrence(needle, haystack):
+    for index in range(len(haystack)):
+        if haystack[index:index + len(needle)] == needle:
+            print(f"{needle} is in {haystack}")
+            print(f"The index is {index}")
+            return index
+    return -1
+
+print(index_of_first_occurrence(needle, haystack))
