8affb9cf1212aa302d0f3efbacce9a25ef4efbc4
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-26 16:42:00 -0700

    First unique char problem

--- strings/first_unique_char.py
+++ strings/first_unique_char.py
@@ -26,4 +26,42 @@ print(first_unique_char(s2))
 print(first_unique_char(s3))
 
 
+def first_unique_char2(s):
+    seen_char_count = {}
 
+    for char in s:
+        if char not in seen_char_count:
+            seen_char_count[char] = 1
+        else:
+            seen_char_count[char] += 1
+    
+    for index, char in enumerate(s):
+        if seen_char_count[char] == 1:
+            return index
+    
+    return -1
+
+print(first_unique_char2(s))
+print(first_unique_char2(s2))
+print(first_unique_char2(s3))
+
+
+
+def first_unique_char3(s):
+    seen_char_count = {}
+
+    for i in range(len(s)):
+        if s[i] not in seen_char_count:
+            seen_char_count[s[i]] = 1
+        else:
+            seen_char_count[s[i]] += 1
+
+    for i in range(len(s)):
+        if seen_char_count[s[i]] == 1:
+            return i
+        
+    return -1
+
+print(first_unique_char3(s))
+print(first_unique_char3(s2))
+print(first_unique_char3(s3))
