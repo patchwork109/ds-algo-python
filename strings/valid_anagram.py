b78d58d0d7d76cd43f8705c28e8f79cfb45b0b6c
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-27 17:49:00 -0700

    Valid anagram update using hash map

--- strings/valid_anagram.py
+++ strings/valid_anagram.py
@@ -7,14 +7,46 @@
 s = "anagram"
 t = "nagaram"
 
-def valid_anagram2(s,t):
+s2 = "rat"
+t2 = "car"
+
+def valid_anagram(s,t):
     if len(s) != len(t):
         return False
     
     return sorted(s) == sorted(t)
 
+print(valid_anagram(s,t))
+
+
+def valid_anagram2(s,t):
+    if len(s) != len(t):
+        return False
+    
+    s_char_count = {}
+    t_char_count = {}
+
+    for char in s:
+        if char not in s_char_count:
+            s_char_count[char] = 1
+        else:
+            s_char_count[char] += 1
+
+    for char in t:
+        if char not in t_char_count:
+            t_char_count[char] = 1
+        else:
+            t_char_count[char] += 1
+    
+    if s_char_count == t_char_count:
+        return True
+    else:
+        return False
+
 
 print(valid_anagram2(s,t))
+print(valid_anagram2(s2,t2))
+
     
 
 
