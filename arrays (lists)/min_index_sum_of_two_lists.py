6e2feeff6a54fa53add29ca31ae1eac9a193b5ff
Samantha Genevay <samanthagenevay@gmail.com>
2023-07-30 14:59:21 -0700

    Min index sum of two lists

---
+++ arrays (lists)/min_index_sum_of_two_lists.py
@@ -0,0 +1,50 @@
+# https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+# Given two arrays of strings list1 and list2, find the common strings with 
+# the least index sum. A common string is a string that appeared in both 
+# list1 and list2. A common string with the least index sum is a common string 
+# such that if it appeared at list1[i] and list2[j] then i + j should be the 
+# minimum value among all the other common strings.
+
+# Return all the common strings with the least index sum. Return the answer 
+# in any order.
+
+# list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+# list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+# --> ["Shogun"]
+
+# list1 = ["happy","sad","good"], list2 = ["sad","happy","good"] --> ["sad","happy"]
+
+list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+
+list3 = ["happy","sad","good"]
+list4 = ["sad","happy","good"]
+
+
+def min_index_sum(list1, list2):
+    common_strings = {}
+    min_index_sum = float('inf')
+
+    list1_indices = {}
+    for index, value in enumerate(list1):
+        list1_indices[value] = index
+    
+    for j, element in enumerate(list2):
+        if element in list1_indices:
+            index_sum = j + list1_indices[element]
+
+            if index_sum < min_index_sum:
+                min_index_sum = index_sum
+
+            common_strings[element] = index_sum
+
+    result = []
+    for string, index_sum in common_strings.items():
+        if index_sum == min_index_sum:
+            result.append(string)
+
+    return result
+    
+print(min_index_sum(list1, list2))
+print(min_index_sum(list3, list4))
+
